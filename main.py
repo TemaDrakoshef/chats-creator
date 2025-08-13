b1d6da321d63c067e2f2f0990911d162540053f2
TemaDrakoshef <artem125149@gmail.com>
2025-08-13 11:45:36 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import asyncio
+import logging
 from pathlib import Path
 
 from telethon import TelegramClient
 
-from config import TELEGRAM_API_ID, TELEGRAM_API_HASH, SESSION_NAME
-
+from config import SESSION_NAME, TELEGRAM_API_HASH, TELEGRAM_API_ID
+from src.logging import setup_logging
 
 client = TelegramClient(
     Path("sessions", SESSION_NAME),
     api_id=TELEGRAM_API_ID,
     api_hash=TELEGRAM_API_HASH,
 )
+logger = logging.getLogger(__name__)
 
 
 async def main():
-    print(TELEGRAM_API_ID)
+    setup_logging(level=logging.INFO)
+    logger.info(TELEGRAM_API_ID)
 
 
 if __name__ == "__main__":
---
+++ src/logging.py
@@ -0,0 +1,12 @@
+import logging
+
+import betterlogging as bl
+
+
+def setup_logging(level=logging.INFO):
+    """Sets up logging for the application."""
+    bl.basic_colorized_config(level=level)
+    logging.basicConfig(
+        level=level,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
