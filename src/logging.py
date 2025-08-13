28ecd7372349c882dacf70fd5b68c34b8df8519d
TemaDrakoshef <artem125149@gmail.com>
2025-08-13 12:19:26 +0300

    Remove telethon logs

--- src/logging.py
+++ src/logging.py
@@ -6,6 +6,9 @@ import betterlogging as bl
 def setup_logging(level=logging.INFO):
     """Sets up logging for the application."""
     bl.basic_colorized_config(level=level)
+    logging.getLogger("telethon.network.mtprotosender").setLevel(
+        logging.WARNING
+    )
     logging.basicConfig(
         level=level,
         format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
